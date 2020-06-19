c0355806454be0b69227466b5b341c505ef378db
defrindr <defrindr@gmail.com>
2020-06-19 16:13:29 +0700

    add dependent dropdown

--- backend/views/pelanggaran/_form.php
+++ backend/views/pelanggaran/_form.php
@@ -14,16 +14,44 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->errorSummary($model); ?>
+    <?= '<label class="control-label">Kelas</label>'; ?>
+    <?= \kartik\widgets\Select2::widget([
+        'name' => 'id_kelas',
+        'data' => \yii\helpers\ArrayHelper::map(
+            \common\models\Kelas::find()
+                ->joinWith(['namaKelas'])
+                ->where(['status' => 1])
+                ->orderBy('nama_kelas')
+                ->asArray()
+                ->all(), 
+            'id_kelas', 
+            'namaKelas.nama_kelas'),
+        'options' => ['id' => 'id_kelas', 'placeholder' => 'Pilih Kelas'],
+        'pluginOptions' => [
+            'allowClear' => true
+        ],
+    ]); ?>
 
     <?= $form->field($model, 'id_pelanggaran', ['template' => '{input}'])->textInput(['style' => 'display:none']); ?>
+    
+    <?= $form->field($model, 'id_siswa')->widget(\kartik\widgets\DepDrop::classname(), [
+        'type' => \kartik\widgets\DepDrop::TYPE_SELECT2,
+        'select2Options' => ['pluginOptions' => ['allowClear' => true]],
+        'options'=>['id'=>'id_siswa'],
+        'pluginOptions'=>[
+            'depends'=>['id_kelas'],
+            'placeholder'=>'Pilih Siswa...',
+            'url'=>\yii\helpers\Url::to(['/v9/siswa'])
+        ]
+    ]); ?>
 
-    <?= $form->field($model, 'id_siswa')->widget(\kartik\widgets\Select2::classname(), [
+    <?php /* $form->field($model, 'id_siswa')->widget(\kartik\widgets\Select2::classname(), [
         'data' => \yii\helpers\ArrayHelper::map(\common\models\Siswa::find()->orderBy('id_siswa')->asArray()->all(), 'id_siswa', 'nama_siswa'),
         'options' => ['placeholder' => 'Pilih Siswa'],
         'pluginOptions' => [
             'allowClear' => true
         ],
-    ]); ?>
+    ]); */?>
 
     <?= $form->field($model, 'id_aturan')->widget(\kartik\widgets\Select2::classname(), [
         'data' => \yii\helpers\ArrayHelper::map(\common\models\Aturan::find()->joinWith('kategori')->orderBy('id_aturan')->asArray()->all(), 'id_aturan', 'uraian_aturan','kategori.kategori_aturan'),
