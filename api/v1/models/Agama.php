15d68ea83e712fb32f1e7c8444fe3dfeb829f765
defrindr <defrindr@gmail.com>
2020-07-17 16:02:19 +0700

    fix typo

--- api/v1/models/Agama.php
+++ api/v1/models/Agama.php
@@ -6,6 +6,6 @@ use common\models\Agama as AgamaBase;
 
 class Agama extends AgamaBase {
     public static function find(){
-        return new \app\v1\models\query\AgamaQuery(get_called_class());
+        return new \api\v1\models\query\AgamaQuery(get_called_class());
     }
 }
